0e012e942c7766e0a4bbfd759e25eb042932ed09
Ayush Kumar Bhadani <ayushkumarbhadani@gmail.com>
2022-11-21 04:26:54 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,256 @@
-import {useState,useRef} from 'react';
+import React,{useState,useRef,useEffect} from 'react';
 import "./App.css";
 import { FaSearch,FaPlus } from 'react-icons/fa';
 import { ImCross } from 'react-icons/im';
 const App = () => {
-  const colorRef=useRef(null);
+  const countTextBox=useRef(null);
   const menuRef=useRef(null);
   const countRef=useRef(null);
+  const saveRef=useRef(null);
+  const [defaultData,setDefaultData]=useState({
+    "data": [
+      {
+        "id": "1",
+        "name": "Bronze",
+        "code": "#CD7F32",
+        "data": [
+          {
+            "quality": "SYMBOLIZES",
+            "traits": [
+              "STRENGTH",
+              "LOYALTY",
+              "SUPPORT",
+              "STABILITY"
+            ]
+          },
+          {
+            "quality": "EFFECTS",
+            "traits": [
+              "SOOTHES",
+              "INSPIRES",
+              "UPLIFTS",
+              "MOTIVATES"
+            ]
+          },
+          {
+            "quality": "POSITIVE",
+            "traits": [
+              "COMFORTING",
+              "RELIABLE",
+              "ELEGANT",
+              "TRUTHFUL"
+            ]
+          },
+          {
+            "quality": "NEGATIVE",
+            "traits": [
+              "BOASTFUL",
+              "CHEAP",
+              "UPTIGHT",
+              "TIGHT-FISTED"
+            ]
+          }
+        ]
+      },
+      {
+        "id": "2",
+        "name": "Lilac",
+        "code": "#C8A2C8",
+        "data": [
+          {
+            "quality": "SYMBOLIZES",
+            "traits": [
+              "HELPFULNESS",
+              "KINDNESS",
+              "SERENITY",
+              "COMPASSION"
+            ]
+          },
+          {
+            "quality": "EFFECTS",
+            "traits": [
+              "RELAXES",
+              "INSPIRES",
+              "UPLIFTS",
+              "SOOTHES"
+            ]
+          },
+          {
+            "quality": "POSITIVE",
+            "traits": [
+              "EMPATHETIC",
+              "FEARLESS",
+              "HELPFUL",
+              "OPTIMISTIC"
+            ]
+          },
+          {
+            "quality": "NEGATIVE",
+            "traits": [
+              "INEXPERIENCED",
+              "FRAGILE",
+              "INFANTILE",
+              "UNPREDICTABLE"
+            ]
+          }
+        ]
+      },
+      {
+        "id": "3",
+        "name": "Yellow",
+        "code": "#F8ED62",
+        "data": [
+          {
+            "quality": "SYMBOLIZES",
+            "traits": [
+              "HAPPINESS",
+              "OPTIMISM",
+              "POSITIVITY",
+              "INTELLECT"
+            ]
+          },
+          {
+            "quality": "EFFECTS",
+            "traits": [
+              "CLARIFIES",
+              "INSPIRES",
+              "AMUSES",
+              "ENERGIZES"
+            ]
+          },
+          {
+            "quality": "POSITIVE",
+            "traits": [
+              "CREATIVITY",
+              "PERCEPTION",
+              "MENTALITY",
+              "WARMTH"
+            ]
+          },
+          {
+            "quality": "NEGATIVE",
+            "traits": [
+              "COWARDICE",
+              "DECEPTION",
+              "EGOTISM",
+              "CAUTION"
+            ]
+          }
+        ]
+      },
+      {
+        "id": "4",
+        "name": "Red",
+        "code": "#FF0000",
+        "data": [
+          {
+            "quality": "SYMBOLIZES",
+            "traits": [
+              "ACTION",
+              "STRENGTH",
+              "ENERGY",
+              "PASSION"
+            ]
+          },
+          {
+            "quality": "EFFECTS",
+            "traits": [
+              "ATTENTION",
+              "MOTIVATES",
+              "STIMULATES",
+              "CAUTIONS"
+            ]
+          },
+          {
+            "quality": "POSITIVE",
+            "traits": [
+              "SEXUALITY",
+              "COURAGE",
+              "DESIRE",
+              "CONFIDENCE"
+            ]
+          },
+          {
+            "quality": "NEGATIVE",
+            "traits": [
+              "ANGER",
+              "DANGER",
+              "REVENGE",
+              "AGGRESSION"
+            ]
+          }
+        ]
+      },
+      {
+        "id": "5",
+        "name": "Blue",
+        "code": "#0033FF",
+        "data": [
+          {
+            "quality": "SYMBOLIZES",
+            "traits": [
+              "SECURITY",
+              "TRUST",
+              "LOYALTY",
+              "RESPONSIBLE"
+            ]
+          },
+          {
+            "quality": "EFFECTS",
+            "traits": [
+              "PROTECTS",
+              "CALMS",
+              "RELAXES",
+              "SUPPORTS"
+            ]
+          },
+          {
+            "quality": "POSITIVE",
+            "traits": [
+              "CONFIDENCE",
+              "PEACE",
+              "HONESTY",
+              "RELIABILITY"
+            ]
+          },
+          {
+            "quality": "NEGATIVE",
+            "traits": [
+              "CONSERVATIVE",
+              "PASSIVE",
+              "DEPRESSED",
+              "PREDICTABLE"
+            ]
+          }
+        ]
+      }
+    ]
+  });
   const [current,setCurrent]=useState({
     color:"#CD7F32",
     title:"The Blue Carbuncle",
     description:"The color of Passion and Energy."
   });
   const [allCard,setAllCard]=useState([]);
-  const closeMenu=()=>{
+  useEffect(()=>{
     menuRef.current.style.display="none";
+    countRef.current.innerHTML=countTextBox.current.value.length + "/300";
+    saveRef.current.style.color=current.color;
+  },[]);
+  const closeMenu=()=>{
+    menuRef.current.style.opacity="0";
+    setTimeout(()=>menuRef.current.style.display="none",500);
+    
   }
   const showMenu=()=>{
     menuRef.current.style.display="block";
+    menuRef.current.style.opacity="100%";
   }
   const selectColor=(e)=>{
     setCurrent(old=>{
       return { ...old, color:e.target.value};
     });
+    saveRef.current.style.color=e.target.value;
   }
   const currentTitle=(e)=>{
     setCurrent(old=>{
@@ -40,7 +269,20 @@ const App = () => {
   const saveCurrent=()=>{
     setAllCard(old=>{
       return [...old,current];
-    })
+    });
+    closeMenu();
+  }
+  const showMore=(e)=>{
+    console.dir(e.target)
+    if(e.target.previousElementSibling.style.display==="block")
+      e.target.previousElementSibling.style.display="none";
+    else if(e.target.previousElementSibling.style.display==="none")
+      e.target.previousElementSibling.style.display="block";
+    else{
+      console.log("Here!");
+      e.target.previousElementSibling.style.display="block";
+    }
+
   }
   return (
     <section>
@@ -80,11 +322,11 @@ const App = () => {
         </div>
         <div className='description'>
           <div>Discription</div>
-          <textarea onChange={(e)=>{currentDescription(e)}} value={current.description}></textarea>
+          <textarea onChange={(e)=>{currentDescription(e)}} value={current.description} ref={countTextBox}></textarea>
           <span className='wordCount' ref={countRef}>0/300</span>
         </div>
         <div className='saveAndCancleBtns'>
-          <button onClick={()=>{saveCurrent();}} className="saveBtn">
+          <button onClick={()=>{saveCurrent();}} className="saveBtn" ref={saveRef}>
             Save
           </button>
           <button className="cancleBtn" onClick={closeMenu}>
@@ -97,8 +339,39 @@ const App = () => {
         allCard.map((item,index)=>{
           return(
             <div className='card' key={index}>
-              <div style={{backgroundColor:item.color, height:"100px",width:"100px", borderRadius:"1rem"}}></div>
-              <div className='cardDesc'> {item.description}</div>
+              <div className='cardUpperPart'>
+                <div style={{backgroundColor:item.color}} className="cardColorBox"></div>
+                <div className='cardDesc'> {item.description}</div>
+              </div>
+              <div className='hiddenDesc'>
+                <table>
+                  <tbody>
+                    <tr>
+                      {defaultData.data.map((data)=>{
+                        if(data.code===item.color){
+                          return data.data.map((datas,index)=>{
+                            return <td className='th' key={index}>{datas.quality}</td>
+                          });
+                        }
+                      })}
+                    </tr>
+                     {  
+                      defaultData.data.filter(s=>s.code===item.color).map((m,n)=>{
+                        return m.data.map((x,i)=>{
+                          return <tr key={i}>
+                              {m.data.map((y,j)=>{
+                                return <td key={j} className="dataTable">{m.data[j].traits[i]}</td>
+                              })
+                        }
+                          </tr>
+                        });
+                      })   
+                    }
+                  </tbody>
+                </table>
+                <div className='titleBox' style={{backgroundColor:item.color}}>Title</div>
+              </div>
+              <button className='showMoreBtn' onClick={(e)=>{showMore(e);}}>Show More</button>
             </div>
           )
         })
@@ -106,7 +379,5 @@ const App = () => {
       </div>
     </section>
   )
-
 }
-
 export default App;
